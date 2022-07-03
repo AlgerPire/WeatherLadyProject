84af3671f78f45bcb5e29fa913b34975fb66cada
AlgerPire <58697445+AlgerPire@users.noreply.github.com>
2022-07-03 13:41:33 +0200

    "Delete Location + List All"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,13 @@
+import entities.Info;
 import services.InfoImplementation;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main( String[] args)
     {
-        InfoImplementation.addLocation();
+//       InfoImplementation.addLocation();
+//        InfoImplementation.deleteLocationById(8);
+  //        InfoImplementation.listAllData();
     }
 }
--- src/main/java/services/InfoImplementation.java
+++ src/main/java/services/InfoImplementation.java
@@ -5,6 +5,10 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class InfoImplementation {
 
     static Session session;
@@ -18,31 +22,75 @@ public class InfoImplementation {
         return sessionFactory;
     }
 
-//    Option 1 - Add location
-    public static void addLocation(){
-        Info info=null;
-        try{
-            session=buildSessionFactory().openSession();
+    //    Option 1 - Add location
+    public static void addLocation() {
+        Info info = null;
+        try {
+            session = buildSessionFactory().openSession();
             session.beginTransaction();
             info = new Info();
-           info.setQyteti("Tirana");
-           info.setShteti("Albania");
+            info.setQyteti("Prague");
+            info.setShteti("Czechia");
             session.save(info);
             session.getTransaction().commit();
-        }
-        catch (Exception e){
+            System.out.println("Added successfully");
+        } catch (Exception e) {
             e.printStackTrace();
 
+
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    //Option 2- Show All Location
+    public static void listAllData( ){
+        Info info = null;
+        try {
+            session = buildSessionFactory().openSession();
+            session.beginTransaction();
+            List data = session.createQuery("FROM Info").list();
+            for (Iterator iterator1 = data.iterator(); iterator1.hasNext();){
+                Info info1 = (Info) iterator1.next();
+                System.out.print("Id: " + info1.getId());
+                System.out.print(" Qyteti: " + info1.getQyteti());
+                System.out.println(" Shteti: "+info1.getShteti());
+            }
+        }  catch (Exception e){
+            e.printStackTrace();
         }
         finally {
-            if(session!=null){
+            if (session!=null){
                 session.close();
             }
         }
     }
 
 
+    // Option 3 - Delete Location
+    public static Info deleteLocationById(int idNumber) {
+        Info info = null;
+        try {
+            session = buildSessionFactory().openSession();
+            session.beginTransaction();
+            info = (Info) session.get(Info.class, idNumber);
+            session.delete(info);
+            session.getTransaction().commit();
+            System.out.println("Deleted successfully");
 
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Id not found");
+            if (null != session.getTransaction()) {
+                session.getTransaction().rollback();
+            }
+            e.printStackTrace();
+        }
+        return info;
+
+    }
 
 
 }
