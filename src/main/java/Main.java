b7fbeb2ed2a63dec3e429805ea7de82c5f66fa54
AlgerPire <58697445+AlgerPire@users.noreply.github.com>
2022-07-03 16:04:46 +0200

    "Final Application"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,12 +5,40 @@ import services.InfoImplementation;
 import java.util.Scanner;
 
 public class Main {
-    public static void main( String[] args)
-    {
-//       InfoImplementation.addLocation();
-//        InfoImplementation.deleteLocationById(8);
-  //        InfoImplementation.listAllData();
-        HttpTest httpTest=new HttpTest();
-        httpTest.getWeatherDataFromApi("Tirana");
+    public static void main(String[] args) {
+
+        System.out.println(
+                "Weather Menu \n" +
+                        "1- Add Location \n" +
+                        "2- Delete Location \n" +
+                        "3- Show all Location \n" +
+                        "4- Get Weather By Location \n" +
+                        "0- Exit from App");
+       Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+            switch (n) {
+                case 1:
+                    InfoImplementation.addLocation();
+                    break;
+                case 2:
+                    InfoImplementation.deleteLocationById();
+                    break;
+                case 3:
+                    InfoImplementation.listAllData();
+                    break;
+                case 4:
+                    HttpTest httpTest = new HttpTest();
+                    Scanner scanner1 = new Scanner(System.in);
+                    System.out.println("Jepni qytetin");
+                    httpTest.getWeatherDataFromApi(scanner1.nextLine());
+                case 0:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid Menu!");
+                    break;
+//               }
+            }
+        }
+
     }
-}
--- src/main/java/services/InfoImplementation.java
+++ src/main/java/services/InfoImplementation.java
@@ -8,6 +8,8 @@ import org.hibernate.cfg.Configuration;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
 
 public class InfoImplementation {
 
@@ -15,6 +17,8 @@ public class InfoImplementation {
     static SessionFactory sessionFactory;
 
     private static SessionFactory buildSessionFactory() {
+        // Turn Hibernate logging off
+        java.util.logging.Logger.getLogger("org.hibernate").setLevel(Level.OFF);
         final SessionFactory sessionFactory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Info.class)
@@ -24,16 +28,20 @@ public class InfoImplementation {
 
     //    Option 1 - Add location
     public static void addLocation() {
+        Scanner scanner = new Scanner(System.in);
         Info info = null;
         try {
             session = buildSessionFactory().openSession();
             session.beginTransaction();
             info = new Info();
-            info.setQyteti("Prague");
-            info.setShteti("Czechia");
+            System.out.print("Vendos Qyetetin: ");
+            info.setQyteti(scanner.nextLine());
+            System.out.print("Vendos Shtetin: ");
+            info.setShteti(scanner.nextLine());
             session.save(info);
+
             session.getTransaction().commit();
-            System.out.println("Added successfully");
+            System.out.println("Shtuar ne databaze");
         } catch (Exception e) {
             e.printStackTrace();
 
@@ -70,19 +78,21 @@ public class InfoImplementation {
 
 
     // Option 3 - Delete Location
-    public static Info deleteLocationById(int idNumber) {
+    public static Info deleteLocationById() {
         Info info = null;
         try {
+            Scanner scanner=new Scanner(System.in);
             session = buildSessionFactory().openSession();
             session.beginTransaction();
-            info = (Info) session.get(Info.class, idNumber);
+            System.out.println("Jepni ID");
+            info = (Info) session.get(Info.class,scanner.nextInt());
             session.delete(info);
             session.getTransaction().commit();
-            System.out.println("Deleted successfully");
+            System.out.println("Fshirja u krye me success");
 
         } catch (Exception e) {
             e.printStackTrace();
-            System.out.println("Id not found");
+            System.out.println("Id nuk gjendet");
             if (null != session.getTransaction()) {
                 session.getTransaction().rollback();
             }
